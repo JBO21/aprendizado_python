9e7687fb7c06d7b1d6617fd149b20d82b3a6e8ee
JBO21 <jackson.jbo@gmail.com>
2022-06-09 12:42:59 -0300

    novo projeto, acrescentando1

--- Jokenpo/main.py
+++ Jokenpo/main.py
@@ -11,9 +11,20 @@ co4 = "#34eb3d"   # green / verde
 co5 = "#e85151"   # red / vermelha
 fundo = "#3b3b3b"
 
+# configuração da Janela
 janela = Tk()
 janela.title('')
 janela.geometry('260x280')
 janela.configure(bg=fundo)
+
+#divisão janela
+top_tela = Frame(janela, width=260, height=100, bg=co1, relief='raised')
+top_tela.grid(row=0, column=0, sticky=NW)
+top_bot = Frame(janela, width=260, height=200, bg=co0, relief='flat')
+top_bot.grid(row=1, column=0, sticky=NW)
+
+estilo = ttk.Style(janela)
+estilo.theme_use('clam')
+
 
 janela.mainloop()
